6f1e3035e2bb437dcab425ed9b618ff71e51ea89
hw <2724363314@qq.com>
2025-02-19 16:10:03 +0800

    finish lesson 4

---
+++ hardhat.config.js
@@ -0,0 +1,29 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@chainlink/env-enc").config()
+require("./tasks")
+//这里要为终端设置代理
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:7890");
+setGlobalDispatcher(proxyAgent);
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const PRIVATE_KEY_1 = process.env.PRIVATE_KEY_1
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY, PRIVATE_KEY_1],
+      chainId: 11155111
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  }
+};
